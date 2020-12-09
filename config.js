03c25fb6a192ecd371a42b5a8578959c9491f478
haxfylpo <75734972+haxfylpo@users.noreply.github.com>
2020-12-09 09:56:20 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    consumer_key:'ck0m2mDOuHJ49U1l8rDpPeBeB',
+    
+    consumer_secret:'PdUMmWd0I1x51DzZokWQ7sUCMbg9ZJ7PfiLBTlGh1qjvbSM0OC',
+    
+    access_token:'1336311407663525888-WGutH8jQLMIkJHk2Ynhk8hjbuFKk82',
+    
+    access_token_secret:'1vD05SqBeeWz6X8paxOwXdFwCERCRfaACOppgHov9NIK'
+    
+    }
